1340fdba8aba9469f1f240752c5bf3da31f6c1de
Stamenov96 <stefan.stamenov1996@gmail.com>
2014-12-26 14:51:58 +0200

    Task11

--- 05_ChristmasHomework/WebContent/scripts/homework.js
+++ 05_ChristmasHomework/WebContent/scripts/homework.js
@@ -30,6 +30,35 @@ $(document).ready(function() {
 		//Task10
 		$("#col1").before($("#col2"));
 	});
+	
+	
+//Task11
+	function handleError(error) {
+		console.error("error", error, arguments);
+	}
+	
+	function appendToList(list, post) {
+		var newElement = $("<li/>");
+		newElement.text(post.title);
+		list.append(newElement);
+	}
+	function processResponse(response) {
+		var list = $("#posts");
+		var i = 0;
+		$.each(response, function() {
+			appendToList(list, this);
+			if (++i >= 5) {
+				return false;
+			}
+		});
+	}
+	
+	$.ajax("http://jsonplaceholder.typicode.com/posts", {
+	  method: "GET"
+	}).then(processResponse, handleError);
 
+	
+
+	
 	
 });
