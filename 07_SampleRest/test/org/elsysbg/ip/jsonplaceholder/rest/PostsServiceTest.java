0426b3128835fd5c88e9984e7ddc9b3dd4ee9ccc
Stamenov96 <stefan.stamenov1996@gmail.com>
2015-03-12 15:50:58 +0200

    Test for PostsRest, but for PostsService, too (bad)

---
+++ 07_SampleRest/test/org/elsysbg/ip/jsonplaceholder/rest/PostsServiceTest.java
@@ -0,0 +1,43 @@
+package org.elsysbg.ip.jsonplaceholder.rest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.elsysbg.ip.jsonplaceholder.model.Post;
+import org.elsysbg.ip.jsonplaceholder.service.PostsService;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * For unit testing you can use JUnit or TestNG
+ */
+public class PostsServiceTest {
+	private PostsService postsService;
+
+	@Before
+	public void setUp() throws Exception {
+		postsService = new PostsService();
+	}
+
+	@Test
+	public void testCreateGetPost() {
+// not set as required in post model
+//		final User author = new User();
+//		author.setEmail("hello@world");
+//		author.setPassword("secret");
+		final Post post = new Post();
+		post.setTitle("hello");
+		post.setBody("world");
+//		post.setUser(author);
+		final Post result =
+			postsService.createPost(post);
+		assertNotEquals(0, result.getId());
+		
+		final Post fromService =
+			postsService.getPost(post.getId());
+		assertEquals("hello", fromService.getTitle());
+		assertEquals("world", fromService.getBody());
+	}
+	
+	// TODO add more tests (update, delete, increment ID, etc.)
+}
