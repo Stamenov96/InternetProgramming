2d87340eb1d6b91ad2fb7f95e933ac21a2671f7d
Stamenov96 <stefan.stamenov1996@gmail.com>
2015-03-12 15:06:10 +0200

    Sample model from http://jsonplaceholder.typicode.com/

---
+++ 07_SampleRest/src/org/elsysbg/ip/jsonplaceholder/model/Post.java
@@ -0,0 +1,39 @@
+package org.elsysbg.ip.jsonplaceholder.model;
+
+import org.apache.tomcat.jni.User;
+
+public class Post {
+	
+	private long id;
+	private User user;
+	private String title;
+	private String body;
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	
+
+}
