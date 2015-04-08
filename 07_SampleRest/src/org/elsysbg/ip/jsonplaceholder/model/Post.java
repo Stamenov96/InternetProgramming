c62df7c4d617c70e4116468f60be53839c5b7013
Stamenov96 <stefan.stamenov1996@gmail.com>
2015-04-08 16:06:35 +0300

    Revert "OneToMany example with ignored fields"

--- 07_SampleRest/src/org/elsysbg/ip/jsonplaceholder/model/Post.java
+++ 07_SampleRest/src/org/elsysbg/ip/jsonplaceholder/model/Post.java
@@ -8,7 +8,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
 
 @XmlRootElement
 // name specifies the name of the table that will hold this entity
@@ -40,10 +39,6 @@ public class Post {
 		this.id = id;
 	}
 	
-	@XmlTransient
-		// without XmlTransient - there will
-		// be recursion when serializing the author
-		// (user -> posts -> author(user))
 	public User getAuthor() {
 		return author;
 	}
